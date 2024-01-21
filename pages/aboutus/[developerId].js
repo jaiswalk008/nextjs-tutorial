0faf0d6edfe05f04cf248fec0c25b77a83a844d8
Karan Jaiswal <jaiskaran008@gmail.com>
2024-01-21 22:42:47 +0530

    added linking pages

--- pages/aboutus/[developerId].js
+++ pages/aboutus/[developerId].js
@@ -17,6 +17,10 @@ const Developer = () =>{
         ]
     const dev = details.filter((dev) => +devId===dev.id)[0];
     // console.log(dev)
+    /*How Links work internally? 
+    Link renders an anchor tag but when we click on that link,
+     it does not request a new page  instead renders the component that needs 
+     to be rendered and changes the URL */
     return (
         dev? <h1>{dev.name}- {dev.role}</h1>:
         <h1>Developer doesn't exist</h1>
--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,25 @@
 //localhost:3000/aboutus
+import Link from "next/link"
 const About=() =>{
-    return <h1>This is an about page</h1>
+    const details = [
+
+        { id : 1, name: 'Yash', role: 'Senior Developer'},
+        
+        { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+        
+        { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+        
+        ]
+    return(
+        <>
+            <h2>Developers:</h2>
+            <ul>
+                {details.map((element) =>{
+                    return <li key={element.id}><Link href={"/aboutus/"+element.id}>
+                        {element.name}</Link></li>
+                })}
+            </ul>
+        </>
+    )
 }
 export default About;
