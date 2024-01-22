d0a90cc29f06c53ac035cdaa07e2a8394b3f883e
Karan Jaiswal <jaiskaran008@gmail.com>
2024-01-22 17:32:24 +0530

    added static functions

--- pages/[meetUpId]/index.js
+++ pages/[meetUpId]/index.js
@@ -2,11 +2,11 @@ import { useRouter } from "next/router";
 import { DUMMY_MEETUPS } from "../index";
 import MeetupItem from "@/components/meetups/MeetupItem";
 
-const MeetupDetail = () => {
+const MeetupDetail = (props) => {
 
     const router = useRouter();
     const meetupId = router.query.meetUpId;  
-    const meetup = DUMMY_MEETUPS.find(meetup => meetup.id === meetupId);
+    const meetup = props.meetup.find(meetup => meetup.id === meetupId);
     // console.log(meetup);
     return(
         <MeetupItem
@@ -18,5 +18,42 @@ const MeetupDetail = () => {
         showDetails={false}
       />
     )
+}/*In the pages directory, the getStaticPaths function is used
+ to define the dynamic paths that should be pre-rendered at build time. */
+//false-means paths contain all supported meetUpId values.therefore if entereed anything else
+//the user will see 404 error
+//true: (default) Dynamic segments not included in generateStaticParams are generated on demand.
+//- it is used when there are a lot of dynamic routes but we only want to the pre-render the most 
+// used ones and it will pre-generate others when requested
+export async function getStaticPaths(){
+  return {
+    fallback:false,
+    paths:[
+      {
+        params:{
+          meetUpId:'m1'
+        }
+      },
+      {
+        params:{
+          meetUpId:'m2'
+        }
+      },
+      {
+        params:{
+          meetUpId:'m3'
+        }
+      }
+    ]
+  }
+}
+export async function getStaticProps(context){
+  const meetUpId = context.params.meetUpId;
+  console.log(meetUpId);
+  return {
+    props:{
+      meetup:DUMMY_MEETUPS
+    }
+  }
 }
 export default MeetupDetail;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "@/components/meetups/MeetupList";
+// import { useEffect, useState } from "react";
 export const DUMMY_MEETUPS=[
     {
         id:'m1',
@@ -13,9 +14,55 @@ export const DUMMY_MEETUPS=[
         image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/800px-Colosseo_2020.jpg',
         address:"some address4545",
         description:"This is a second meetup"
+    },
+    {
+        id:'m3',
+        title:'A third Meetup',
+        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/800px-Colosseo_2020.jpg',
+        address:"spme addrwes -33333",
+        description:"This is the third meetup"
     }
 ]
-function HomePage(){
-    return <MeetupList meetups={DUMMY_MEETUPS}/>
+function HomePage(props){
+    // const [loadedMeetups,setLoadedMeetups] = useState([]);
+    // useEffect(()=>{
+    //     //react useffect will take place due to which DUMMY_MEETUPS wont appear
+    //     //in site source data so bad SEO, also only the data on the first render
+    //     //goes for SEO
+    //     setLoadedMeetups(DUMMY_MEETUPS);
+    // },[])
+    return <MeetupList meetups={props.meetups}/>
 }
+/* this function is called before the compopnent function
+used for getting the data that the component needs so that the data can be used for pre-rendering
+this function is executed during the BUILD process --> not on the server neither on the client
+should be used when  thje data doesnot changes frequently
+*/
+
+export async function getStaticProps(){
+     //fetch data from api
+     //read file from file system
+     //return an object = has to be named props
+//revalidate runs the function after some time as provided so that the data gets updated.
+     return {
+        props:{
+            meetups:DUMMY_MEETUPS,
+        },
+        revalidate:10
+     }; 
+}
+
+// export async function getServerSideProps(context){
+//     //fetch data from api or db
+//     //runs on server so can run server side code
+        //can do authentication
+//     const req= context.req;
+//     const res= context.res;
+//     return {
+//         props:{
+//             meetups:DUMMY_MEETUPS,
+//         }
+//     }
+// }
+
 export default HomePage;
